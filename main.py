523b8fb05f72e2bef446a4493c2477a77fb92a37
Carlos Alfredo Montoya Goez <CarlosMontoya@comfama.com.co>
2022-02-25 19:20:11 -0500

    NTV002 Se agrego variable, se recibio el dato por consola y se imprime

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 #Comentario   versionamiento NTV001 se crea main.py para codificar condicionales
 print('Que miendo la guerra');
+
+
+nivelDeAgua = int(input('Digita la cantidad de agua de HidroItuango :'))
+print(f'El nivel del agua es {nivelDeAgua}')
