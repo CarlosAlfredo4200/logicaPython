2cb4102785246a5b6acd1daef84fa79a7480c3db
Carlos Alfredo Montoya Goez <CarlosMontoya@comfama.com.co>
2022-02-25 20:55:59 -0500

    NTV001 ejercicio de nivel de agua con tres resultados

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #Comentario   versionamiento NTV001 se crea main.py para codificar condicionales
-print('Que miendo la guerra');
-
-
+ 
 nivelDeAgua = int(input('Digita la cantidad de agua de HidroItuango :'))
 print(f'El nivel del agua es {nivelDeAgua}')
 
-## nuevo comentario
-
-print('Ohe')
-
-#comentario
+if(nivelDeAgua < 200 ):
+    print('Niveles bajos')
+elif(nivelDeAgua >= 200 and nivelDeAgua < 450 ):
+    print('Niveles optimos')
+else :
+    print('Niveles altos')
+        
+ 
