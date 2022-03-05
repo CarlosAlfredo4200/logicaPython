aae4b3cde86d72987ece05547f060c59ddf185cf
Carlos Alfredo Montoya Goez <CarlosMontoya@comfama.com.co>
2022-03-04 21:06:49 -0500

    Adicion de listas b

--- listas.py
+++ listas.py
@@ -1,5 +1,8 @@
-numeros = [1,2,3,4,5,6,7,8,9]
-print(numeros)
+# numeros = [1,2,3,4,5,6,7,8,9]
+# #print(numeros)
 
-for numero in numeros:
-    print(numero)
+# for numero in numeros:
+#      print(numero)
+    
+for k in range(1, 5, 2):
+    print(k)
